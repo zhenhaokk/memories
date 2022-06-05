e207f170a3e7ec2d7075dbf6c0b51cb147d5a1e5
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-06-05 22:30:24 +1000

    se

--- server/index.js
+++ server/index.js
@@ -2,3 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+app.use('/posts', postRoutes);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://zhenhao:zhenhao123123@cluster0.y3i37.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => app.listen(PORT, () => console.log(`Server started on port ${PORT}`)))
+  .catch((error) => console.log(error));
+
+mongoose.set("useFindAndModify", false);
