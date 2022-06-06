aa755120aba738d01ec70ba7a4dd64e8348ff3ab
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-06-06 17:26:39 +1000

    work4

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ import cors from "cors";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-app.use('/posts', postRoutes);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://zhenhao:zhenhao123123@cluster0.y3i37.mongodb.net/?retryWrites=true&w=majority";
