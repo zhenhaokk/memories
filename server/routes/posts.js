d16e4f19639bc14227e41c5bc32475b1e57de897
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-06-10 23:42:44 +1000

    commentwork

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -8,6 +8,7 @@ import {
   updatePost,
   likePost,
   deletePost,
+  commentPost,
 } from "../controllers/posts.js";
 
 const router = express.Router();
@@ -21,5 +22,6 @@ router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
 router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", commentPost);
 
 export default router;
